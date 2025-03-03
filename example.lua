bba8e49f57b32afc2c253dbbd69bf7d4899395cf
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2025-03-03 21:04:41 +0700

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,9 +1,9 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/lagbackisasussydog/ui_lib/refs/heads/main/main.lua"))
 
-local win = lib.Init(Vector2.new(500,325),Enum.KeyCode.LeftAlt)
+local win = lib:Init(Vector2.new(500,325),Enum.KeyCode.LeftAlt)
 
 win.SetWindowIcon("10800748303")
-local autofarm = win.CreateButton("AutoFarm","16927814188")
-win.CreateElement("Button",autofarm,"Test button")
-win.CreateElement("Toggle",autofarm,"Toggle")
-win.CreateElement("Label",autofarm,"This is a text label")
+local test = win.CreateButton("AutoFarm","16927814188")
+win.CreateElement("Button",test,"Test button")
+win.CreateElement("Toggle",test,"Toggle")
+win.CreateElement("Label",test,"This is a text label")
