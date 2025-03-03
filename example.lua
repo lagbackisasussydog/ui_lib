8d7e6a435e3cdaf9d3814a0ee08cae777b82d5a4
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2025-03-03 17:21:07 +0700

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,28 +1,9 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/lagbackisasussydog/ui_lib/refs/heads/main/main.lua"))
 
-local newGui = lib.Get()
+local win = lib.Init(Vector2.new(500,325),Enum.KeyCode.LeftAlt)
 
-local autofarm = lib.CreateButton("AutoFarm","AutoFarm","9695653110") -- Create button
-local travel = lib.CreateButton("Travel","Travel","6026568198")
-local setting = lib.CreateButton("Settings","Settings","6031280882")
-local deb = lib.CreateButton("Dev","Debug","6022852107")
-
-lib.SetGuiIcon("5205790785") -- Set UI icon
-lib.Send("Initializing script",10) -- Send a notification
-
-local btn1 = lib.CreatePanelElement("Button",autofarm,"Enable") -- Get button
-lib.CreatePanelElement("Label",autofarm,"This is a text label")
-btn1.MouseButton1Up:Connect(function() -- You can also use lib.Connect(button,function() end)
-	print("Enabled")
-end)
-
-local btn2 = lib.CreatePanelElement("Toggle",travel,"Somewhere")
-btn2.MouseButton1Up:Connect(function()
-  -- Make your own boolean switch system
-	print("Hi")
-end)
-
-local btn3 = lib.CreatePanelElement("Button",deb,"Inf Yield")
-btn3.MouseButton1Up:Connect(function()
-	loadstring("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source")()
-end)
+win.SetWindowIcon("10800748303")
+local autofarm = win.CreateButton("AutoFarm","16927814188")
+win.CreateElement("Button",autofarm,"Test button")
+win.CreateElement("Toggle",autofarm,"Toggle")
+win.CreateElement("Label",autofarm,"This is a text label")
