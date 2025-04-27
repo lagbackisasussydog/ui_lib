9024a1e88be587656cb61fc6deaf65889512a91e
lagbackisasussydog <107791485+lagbackisasussydog@users.noreply.github.com>
2025-04-27 09:31:49 +0700

    Update example.lua

--- example.lua
+++ example.lua
@@ -7,3 +7,14 @@ local test = win:CreateButton("AutoFarm","16927814188")
 win:CreateElement("Button",test,"Test button")
 win:CreateElement("Toggle",test,"Toggle")
 win:CreateElement("Label",test,"This is a text label")
+win:CreateElement("TextBox",test,"This is a non-number box")
+win:CreateElement("NumberBox",test,"This is a number-only box")
+win:CreateElement("Slider",test,{
+    ["text"] = "This is a slider",
+    ["start"] = 1,
+    ["max"] = 100
+})
+win:CreateElement("Notification",test,{
+    ["Text"] = "This is a notification",
+    ["Duration"] = 5
+})
